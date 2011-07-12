8435809a8c5e981b630750549a16a2ffa77e8136
zentooo <ankerasoy@gmail.com>
2011-07-12 22:01:13 +0900

    add backward.js

---
+++ src/backward.js
@@ -0,0 +1,18 @@
+(function(ap) {
+  if ( typeof ap.reduce !== "function" ) {
+    ap.reduce = reduce;
+  }
+  function reduce(iterator, initial) {
+    var sum = initial;
+    if ( typeof iterator !== "function" ) {
+      throw new Error("First parameter should be function");
+    }
+    if ( typeof initial === "undefined" ) {
+      throw new Error("Second parameter should not be undefined");
+    }
+    this.forEach(function(current) {
+      sum = iterator(sum, current);
+    });
+    return sum;
+  }
+})(Array.prototype);
