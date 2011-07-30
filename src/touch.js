9e73a001c1d6c85f894657922cd0ee323e631a28
zentooo <ankerasoy@gmail.com>
2011-07-30 18:16:02 +0900

    looks ok?

--- src/touch.js
+++ src/touch.js
@@ -1,42 +1,55 @@
 r(function() {
 
     var body = document.body,
-    touchEvents = ["swipeLeft", "swipeRight"],
-    touch = { x1: 0, x2: 0 };
-
-    function search(node) {
-        return 'tagName' in node ? node : node.parentNode;
-    }
+        moveThreshold = 30,
+        touchEvents = ["tap", "swipeleft", "swiperight", "swipeup", "swipedown"],
+        touch = { x1: 0, x2: 0, y1: 0, y2: 0, target: null };
 
     function trigger(elem, event) {
-        var e = document.createEvent("Event");
-        e.initEvent(event, true, true);
-        elem.dispatchEvent(e);
+        var evt = document.createEvent("Event");
+        evt.initEvent(event, true, true);
+        elem.dispatchEvent(evt);
     }
 
-    r(body).bind("touchstart", function(e) {
-        touch.target = search(e.touches[0].target);
-        touch.x1 = e.touches[0].pageX;
+    r(body).bind("touchstart", function(evt) {
+        touch.x1 = evt.touches[0].clientX;
+        touch.y1 = evt.touches[0].clientY;
+
+        touch.target = evt.touches[0].target;
     });
 
-    r(body).bind("touchmove", function(e) {
-        touch.x2 = e.touches[0].pageX;
+    r(body).bind("touchmove", function(evt) {
+        touch.x2 = evt.touches[0].clientX;
+        touch.y2 = evt.touches[0].clientY;
     });
 
-    r(body).bind("touchend", function(e) {
-        var delta = touch.x1 - touch.x2;
+    r(body).bind("touchend", function(evt) {
+        var dx = touch.x1 - touch.x2, dy = touch.y1 - touch.y2,
+            adx = Math.abs(dx), ady = Math.abs(dy);
 
-        if ( touch.x1 && touch.x2 && Math.abs(delta) > 30 ) {
-            if ( delta > 0 ) {
-                trigger(touch.target, "swipeLeft");
+        if ( ady > moveThreshold && ady > adx ) {
+            evt.preventDefault();
+            if ( dy >= 0 ) {
+                trigger(touch.target, "swipeup");
             }
             else {
-                trigger(touch.target, "swipeRight");
+                trigger(touch.target, "swipedown");
             }
         }
+        else if ( adx > moveThreshold && adx > ady ) {
+            evt.preventDefault();
+            if ( dx >= 0 ) {
+                trigger(touch.target, "swipeleft");
+            }
+            else {
+                trigger(touch.target, "swiperight");
+            }
+        }
+        else {
+            trigger(touch.target, "tap");
+        }
 
-        touch.target = null;
-        touch.x1 = touch.x2 = 0;
+        touch = { x1: 0, x2: 0, y1: 0, y2: 0, target: null };
     });
 
     touchEvents.forEach(function(event) {
@@ -44,5 +57,4 @@ r(function() {
             this.bind(event, callback, useCapture);
         };
     });
-
 });
--- test/t/touch.js
+++ test/t/touch.js
@@ -1,77 +1,31 @@
 module("test for touch event");
 
-is = strictEqual;
-isnt = notStrictEqual;
+test("just touch with your iPhone/Android devices", function() {
+    ["tap", "doubletap", "swiperight", "swipeleft", "swipeup", "swipedown"].forEach(function(event) {
+        var ts = r("#touchspace");
+        ts.bind(event, function() {
+            alert(event);
+        });
+        var touchspace = document.getElementById("touchspace");
+        touchspace.addEventListener("touchstart", function(evt) {
+            var t = evt.touches[0];
+            if ( Math.random() > 0.7 ) {
+                r(touchspace).add("touchstart: " + t.pageX + ", " + t.pageY + "<br />");
+            }
+        }, false);
+        touchspace.addEventListener("touchmove", function(evt) {
+            var t = evt.touches[0];
+            if ( Math.random() > 0.8 ) {
+                r(touchspace).add("touchmove: " + t.pageX + ", " + t.pageY + "<br />");
+            }
+        }, false);
+        touchspace.addEventListener("touchend", function(evt) {
+            var t = evt.touches[0];
+            r(touchspace).add("touchend: " + t.pageX + ", " + t.pageY + "<br />");
+        }, false);
 
-function emit(el, eventName, callback, touches) {
-  var event = document.createEvent("Event");
-  event.initEvent(eventName, true, true);
-  event.touches = touches;
-  console.dir(touches);
-  console.dir(event);
-  ( el.__proto__ === r.fn ) ? el[0].dispatchEvent(event) : el.dispatchEvent(event);
-
-  if ( typeof callback === "function" ) {
-    callback();
-  }
-}
-
-function testBind(wrapped, eventName) {
-  var spy = sinon.spy(function(e) {
-    ok ( e.target === wrapped[0], "event target original element");
-    ok ( e.type === eventName, "event type should be " + eventName);
-  });
-  wrapped.bind(eventName, spy);
-  emit(wrapped, eventName);
-  ok( spy.calledOnce, eventName + " emitted and callback function called successfully");
-}
-
-test("touch events", function() {
-  var button = r("#button");
-
-  ["touchStart", "touchmove", "touchend"].forEach(function(event) {
-    testBind(button, event);
-  });
-});
-
-
-test("swipe event", function() {
-  var button = r("#button"),
-      startTouches = [ { target: button[0], pageX: 100 } ],
-      moveTouchesLeft = [ { target: document.body, pageX: 50 } ],
-      moveTouchesRight = [ { target: document.body, pageX: 150 } ],
-      spyLeft, spyRight;
-
-  spyLeft = sinon.spy(function(e) {
-    ok( e.target === button[0], "event target is touched button" );
-    ok( e.type === "swipeLeft", "event type is swipeLeft" );
-  });
-
-  button.bind("swipeLeft", spyLeft);
-
-  emit(button[0], "touchstart", function() {
-    emit(document.body, "touchmove", function() {
-      emit(document.body,  "touchend", function() {
-          ok( spyLeft.calledOnce, "swipeLeft happens" );
-      });
-    }, moveTouchesLeft);
-  }, startTouches);
-
-  spyRight = sinon.spy(function(e) {
-    ok( e.target === button[0], "event target is touched button" );
-    ok( e.type === "swipeRight", "event type is swipeRight" );
-  });
-
-  button.bind("swipeRight", spyRight);
-
-  emit(button[0], "touchstart", function() {
-    emit(document.body, "touchmove", function() {
-      emit(document.body,  "touchend", function() {
-          ok( spyRight.calledOnce, "swipeRight happens" );
-      });
-    }, moveTouchesRight);
-  }, startTouches);
-
-  button.unbind("swipeLeft");
-  button.unbind("swipeRight");
+        r("#button").bind(event, function() {
+            ts.html("");
+        });
+    });
 });
